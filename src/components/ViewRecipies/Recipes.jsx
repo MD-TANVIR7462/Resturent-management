7bc1cfca770403f9ef5515826a9d57d6a5f9891e
unknown <mdtanvir7462@gmail.com>
2023-05-03 00:59:10 +0600

    Card section finished

--- src/components/ViewRecipies/Recipes.jsx
+++ src/components/ViewRecipies/Recipes.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 
-const Recipes = ({SingleRecipe}) => {
+const Recipes = ({ SingleRecipe }) => {
    console.log(SingleRecipe)
-   const {recipe_img
-,recipeName,ingredients,cookingMethod,rating
+   const { recipe_img
+      , recipeName, ingredients, cookingMethod, rating
 
-}=SingleRecipe
+   } = SingleRecipe
    return (
       <div className='mb-5 mt-2' >
-<div className="card bg-pink-50 grid md:grid-cols-2 md:w-5/6 md:mx-auto card-side  shadow-xl p-2 m-2 md:px-8">
- 
-  <figure className='rounded-lg'><img src={recipe_img} className=' ' alt="Movie"/></figure>
+         <div className="card bg-pink-50 grid md:grid-cols-2 md:w-5/6 md:mx-auto card-side  shadow-xl p-2 m-2 md:px-8">
 
-  <div className="card-body">
-    <h2 className="card-title mb-2">{recipeName}</h2>
-    <div className='md:flex  justify-center '><span className='text-base font-bold'>Ingradients : </span> <p> {
-     
-       ingredients.map(ingradient=> <li className=' text-sm ps-2'> {ingradient},</li>)
-     
+            <figure className='rounded'><img src={recipe_img} className=' p-2' alt="Movie" /></figure>
 
-    }</p></div>
-    <p className=''> <span className='text-base font-bold'>Cooking-Mathod : </span> {cookingMethod}</p>
-   
-    <div className="card-actions  justify-center pt-3 flex ">
-    <p><span className='font-bold '>Rarings :</span> {rating}</p>
-      <button className="btn btn-primary">Favourite</button>
-    </div>
-  </div>
-</div>
+            <div className="card-body">
+               <h2 className="text-center font-bold text-2xl mb-3">{recipeName}</h2>
+               <div className='md:flex  justify-center '><span className='text-base font-bold'>Ingradients : </span> <p> {
+
+                  ingredients.map(ingradient => <li className=' text-sm ps-2'> {ingradient},</li>)
+
+
+               }</p></div>
+               <p className=''> <span className='text-base font-bold'>Cooking-Mathod : </span> {cookingMethod}</p>
+
+               <div className="card-actions  justify-center pt-3 flex ">
+                  <p><span className='font-bold '>Rarings :</span> {rating}</p>
+
+                  <button type="button" className=" text-xs md:text-sm btn border-none px-5 py-3 rounded-lg text-white font-bold  bg-gradient-to-r from-pink-400 to-purple-900 hover:from-pink-500 hover:to-indigo-950" ><FaHeart></FaHeart> Favourite</button>
+               </div>
+            </div>
+         </div>
       </div>
    );
 };
--- src/components/navBar/Nav.jsx
+++ src/components/navBar/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { FaHome,FaBloggerB,FaSignInAlt } from 'react-icons/fa';
 const Nav = () => {
    const user = true
    return (
@@ -12,10 +13,10 @@ const Nav = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
-              <li><NavLink to={'/'} className={  ({ isActive }) => isActive ? 'text-pink-800 ' : 'text-black pl-4'}>  Home</NavLink ></li>
+              <li><NavLink to={'/'} className={  ({ isActive }) => isActive ? 'text-pink-800 ' : 'text-black pl-4'}><FaHome></FaHome>  Home</NavLink ></li>
 
-              <li> <NavLink to={'/Blogs'} className={({ isActive }) => isActive ? " text-pink-800 pl-4" : "'text-black  pl-4 "}> Blog</NavLink></li>
-              <li> <NavLink to={'/resister'} className={({ isActive }) => isActive ? " text-pink-800 pl-4" : "'text-black  pl-4 "}> Resister</NavLink></li>
+              <li> <NavLink to={'/Blogs'} className={({ isActive }) => isActive ? " text-pink-800 pl-4" : "'text-black  pl-4 "}> <FaBloggerB></FaBloggerB>Blog</NavLink></li>
+              <li> <NavLink to={'/resister'} className={({ isActive }) => isActive ? " text-pink-800 pl-4" : "'text-black  pl-4 "}><FaSignInAlt></FaSignInAlt> Resister</NavLink></li>
             </ul>
           </div>
           <Link to={'/'} className=" font-bold  text-base md:text-3xl"><span className='text-pink-800' >Indian  </span> Cafe</Link>
@@ -24,10 +25,10 @@ const Nav = () => {
         </div>
         <div className="navbar-center hidden lg:flex text-lg ">
           <ul className="menu menu-horizontal  font-bold mx-5">
-            <NavLink to={'/'} className={({ isActive }) => isActive ? ' text-pink-800 mr-5 ' : 'text-black mr-5'}> Home</NavLink>
+            <NavLink to={'/'} className={({ isActive }) => isActive ? ' text-pink-800 mr-5 flex  items-center' : 'text-black mr-5 flex  items-center'}><FaHome></FaHome> Home</NavLink>
 
-            <NavLink to={'/Blogs'} className={({ isActive }) => isActive ? " text-pink-800 pl-4" : "'text-black  pl-5 "}>  Blog</NavLink >
-            <NavLink to={'/resister'} className={({ isActive }) => isActive ? " text-pink-800 pl-4" : "'text-black  pl-5 "}>  Resister</NavLink >
+            <NavLink to={'/Blogs'} className={({ isActive }) => isActive ? " text-pink-800 pl-4 flex  items-center" : "'text-black  pl-5 flex  items-center "}> <FaBloggerB></FaBloggerB> Blog</NavLink >
+            <NavLink to={'/resister'} className={({ isActive }) => isActive ? " text-pink-800 pl-4 flex  items-center" : "'text-black  pl-5 flex  items-center "}> <FaSignInAlt></FaSignInAlt> Resister</NavLink >
            
           </ul>
         </div>
