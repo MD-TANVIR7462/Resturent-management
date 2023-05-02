d36f1804a1686b389b1840827459522a89e8b60b
unknown <mdtanvir7462@gmail.com>
2023-05-02 18:07:48 +0600

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Nav from './components/navBar/Nav';
 import MainHome from './components/Home/Main/MainHome';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className='w-11/12 mx-auto'>
       <Nav></Nav>
-      
      <MainHome></MainHome>
+     <Footer></Footer>
       
     </div>
   );
